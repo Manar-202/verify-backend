6e6603576faf4618a008fcb756247a79fb5a09e4
Manar-202 <119630678+Manar-202@users.noreply.github.com>
2025-05-14 15:58:21 +0300

    all

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+//const session = require('express-session');
+const passport = require('passport');
+require('./config/passport');
+const requestRoutes = require('./routes/requestRoutes');
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use('/uploads', express.static('uploads'));
+app.use(express.urlencoded({ extended: true }));
+
+/*app.use(session({
+  secret: 'manar-super-secret-session-key',
+  resave: false,
+  saveUninitialized: false
+}));*/
+
+app.use(passport.initialize());
+//app.use(passport.session());
+// Routes
+app.use('/api/requests', requestRoutes);
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log('MongoDB connected');
+  app.listen(process.env.PORT, () => {
+    console.log(`Server running on port ${process.env.PORT}`);
+  });
+}).catch(err => console.log(err));
